c2988e0e3b56171df72a76426e5109e3f2adfd48
Neil Derraugh <c_nderraugh@costco.com>
2025-02-28 13:48:11 -0500

    Initial commit of Scala.js app

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite"; 
+import scalaJSMillPlugin from "vite-plugin-scalajs-mill";
+
+export default defineConfig({
+    plugins: [scalaJSMillPlugin({
+        moduleNames: "livechart"
+    })],
+    server: {
+        hmr: true,
+    }
+})
